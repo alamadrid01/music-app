234903f59d6e0ded6ce4c0dde4a8dd1502b5f0a8
oyinkansola <adebayoalameen88@gmail.com>
2022-12-12 20:39:33 +0100

    Feat: Integrated an API

--- src/components/currenPlayer/CurrentPlayer.js
+++ src/components/currenPlayer/CurrentPlayer.js
@@ -1,60 +1,77 @@
-import React,{useState, useEffect} from 'react'
-import './currentPlayer.scss';
-import axios from 'axios'
-import shuffle from './assets/shuffle.svg'
-import play from './assets/play.svg'
-import pause from './assets/pause.svg'
-import previous from './assets/backward.svg'
-import next from './assets/forward.svg'
+import React, { useState, useEffect } from "react";
+import "./currentPlayer.scss";
+import axios from "axios";
+import shuffle from "./assets/shuffle.svg";
+import play from "./assets/play.svg";
+import pause from "./assets/pause.svg";
+import previous from "./assets/backward.svg";
+import next from "./assets/forward.svg";
 
 function CurrentPlayer() {
-    const [artist, setArtist]= useState('');
-    const [title, setTitle]= useState('');
-    const [duration, setDuration]= useState('');
-    const [image, setImage]= useState('');
-    const [data, setData]= useState();
-    const [percentage, setPercentage]= useState(0);
+  const [artist, setArtist] = useState("");
+  const [title, setTitle] = useState("");
+  const [duration, setDuration] = useState("");
+  const [image, setImage] = useState("");
+  const [data, setData] = useState();
+  const [percentage, setPercentage] = useState(0);
 
-    
-    const percentageHandler =(e) =>{
-      setPercentage(e.target.value)
-    }
- 
-  
+  const percentageHandler = (e) => {
+    setPercentage(e.target.value);
+  };
+
+  const options = {
+    method: "GET",
+    url: "https://shazam-core.p.rapidapi.com/v1/charts/world",
+    headers: {
+      "X-RapidAPI-Key": "aa52058a48msha779089f63619d6p10d638jsn24e7f022859c",
+      "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  }, []);
 
   return (
     <div>
-        <div className="currentPlayer">
-          <div className="left">
-            <div className="songImage">
-              <img src={image} alt="picture" />
-              
-            </div>
-            <div className="text">
-              <span>{title}</span>
-              <small>{artist}</small>
-            </div>
+      <div className="currentPlayer">
+        <div className="left">
+          <div className="songImage">
+            <img src={image} alt="aint loading" />
           </div>
-          <div className="middle">
-            <div className="image">
-              <img src={shuffle} alt="" />
-              <img src={previous} alt="" />
-              <img src={play} alt="" />
-              <img src={next} alt="" />
-            </div>
-            <div className="progressBar">
-              <input onChange={percentageHandler} type="range" steps='0.01' id="range" />
-            </div>
-
-          </div>
-          <div className="right">
-            {
-              percentage
-            }
+          <div className="text">
+            <span>{title}</span>
+            <small>{artist}</small>
           </div>
         </div>
+        <div className="middle">
+          <div className="image">
+            <img src={shuffle} alt="" />
+            <img src={previous} alt="" />
+            <img src={play} alt="" />
+            <img src={next} alt="" />
+          </div>
+          <div className="progressBar">
+            <input
+              onChange={percentageHandler}
+              type="range"
+              steps="0.01"
+              id="range"
+            />
+          </div>
+        </div>
+        <div className="right">{percentage}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CurrentPlayer
+export default CurrentPlayer;
